e520847d3be97f982c8124c7d9a06c9ac7785082
sexfat <bryant6831@gmail.com>
2017-01-21 19:23:32 +0800

    update finish

--- js/tween.js
+++ js/tween.js
@@ -1,6 +1,72 @@
 $(function() {
 
+//====================
+//選單
+//====================
 
+
+$('a.page-scroll').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              $('html,body').animate({
+                  scrollTop: target.offset().top
+              }, 1000);
+              return false;
+          }
+      }
+  });
+  console.log('nav ok');
+
+
+  /* ==============================================
+       回到最頂端
+       =============================================== */
+     $(window).scroll(function(){
+       var offset_height = 400;
+       var distance =$(window).scrollTop();
+
+       if (distance > offset_height){
+          $('.gototop').addClass('on');
+       }else {
+         $('.gototop').removeClass('on');
+       }
+     });
+
+     $('.gototop').on('click',function(e){
+       event.preventDefault();
+       $('body,html').animate({
+         scrollTop: 0,
+       },700);
+       console.log('gototop');
+     });
+
+
+     /* ==============================================
+         選單出現
+        =============================================== */
+
+
+        $(window).scroll(function(){
+          var offset_height = 700;
+          var distance =$(window).scrollTop();
+
+          if (distance > offset_height){
+             $('.navbar').addClass('on');
+          }else {
+            $('.navbar').removeClass('on');
+          }
+        });
+
+
+
+
+
+
+
+
+//第一個場景動畫
     TweenMax.fromTo(".section_01 .title", 2, {
         y: -150,
         opacity: 0,
@@ -27,87 +93,93 @@ $(function() {
     var controller = new ScrollMagic.Controller();
     // new 場景
 
-    var wp = new TimelineMax().staggerFromTo(".flow .item", 0.5, {
+    var wp = new TimelineMax().staggerFromTo('.flow .item', 0.5, {
         y: -50,
         opacity: 0
     }, {
         y: 0,
         opacity: 1,
     }, 0.1);
+
+
+
     console.log("time");
     var scene = new ScrollMagic.Scene({
             triggerElement: "#section_01",
             reverse: true,
+            offset:'100px',
+            // duration: '90%'
         })
 
         .setTween(wp)
         .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
-
-
     //  add css
 
     var ourScene = new ScrollMagic.Scene({
-            triggerElement: '#section_02',
-            duration: '80%',
-            offset: '100px'
-        })
-        .setClassToggle('.icon', 'fade-in') //add class to id
-        .addIndicators({
-            name: 'fade scene'
-        })
-        .addTo(controller);
-    console.log("section_02  ok");
-
-    //logo 變背景顏色
-
-    var ourScene_part2 = new ScrollMagic.Scene({
-            triggerElement: '#section_03'
-        })
-        .setClassToggle('.bgc', 'color_fadein') //add class to id
-        .addIndicators({
-            name: 'fade scene color'
-        })
-        .addTo(controller);
-    console.log("section_03  ok");
-
-    //sticky  box
-
-    var sence_sticky = new ScrollMagic.Scene({
-            triggerElement: '#section_04',
-            duration: 300,
-            offset: 100
-        })
-        .setPin("#sticky")
-        .addIndicators({
-            name: 'sticky '
-        })
-        .addTo(controller);
-    console.log("sticky  ok");
-
-//移動字體
-
-var scene = new ScrollMagic.Scene({
-        triggerElement: "#section_05",
-        duration: 400,
-        // offset: 150,
-        //only run once
-        reverse: true
+        triggerElement: "#section_02",
+        offset:'200px'
     })
-    .setTween(TweenMax.staggerTo(".section_06 .title", 1, {
-        opacity: 1,
-        x: 980,
-        ease: Strong.easeInOut
-    }))
-    .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
+     .setClassToggle('.icon','fade-in')
+     .addIndicators({name :'bgc start'})
+     .addTo(controller);
 
+
+// 改變背景顏色
+var bgc_sence = new ScrollMagic.Scene({
+    triggerElement: "#section_03",
+    offset:'100px'
+})
+ .setClassToggle('.bgc','color_fadein')
+ .addIndicators({
+    name :'bgc start'
+ })
+ .addTo(controller);
+
+//sticky box
+
+var scene_sticky = new ScrollMagic.Scene({
+  triggerElement: "#section_04",
+  duration: 200,
+  offset: 10
+})
+
+.setPin("#sticky" ,{spacerClass: 'asd'})
+.addIndicators({
+   name :'sticky start'
+})
+.addTo(controller);
+
+//移動字
+
+var scene_move = new ScrollMagic.Scene({
+
+  triggerElement: "#section_05",
+  duration: 400,
+  reverse: true,
+})
+
+  .setTween(TweenMax.staggerTo(".section_06 .title" , 1 ,{
+      opacity: 1,
+      x : 1350,
+      ease: Strong.easeInOut
+
+  }))
+  .addIndicators({
+     name :'move'
+  })
+.addTo(controller);
+
+console.log('move ok');
 //stellar init
 
+
 $('#container').stellar({
-    scrollProperty: 'scroll',
-    // verticalOffset: 100,
-    responsive: true
+
+  horizontalOffset: 0,
+  verticalOffset: 0,
+
+
 });
 
 
